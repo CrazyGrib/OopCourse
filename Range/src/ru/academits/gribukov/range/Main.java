8c3ad521680ddf3c9cd3b1a2a170789324f33625
Александр Грибуков <a.gribukov@cft.ru>
2023-07-19 11:28:04 +0700

    Написал небольшую программу с использованием класса Range.

--- Range/src/ru/academits/gribukov/range/Main.java
+++ Range/src/ru/academits/gribukov/range/Main.java
@@ -1,7 +1,34 @@
 package ru.academits.gribukov.range;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Range range = new Range(1.5, 9.5);
+
+        System.out.printf("Заданный диапазон имеет значения:%n from = %f%n to = %f%n", range.getFrom(), range.getTo());
+
+        System.out.println("Теперь задвайте зададим свое значение и узнаем длину диапазона:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите вещественное значение from = ");
+        range.setFrom(scanner.nextDouble());
+
+        System.out.print("Введите вещественное значение to = ");
+        range.setTo(scanner.nextDouble());
+
+        System.out.printf("Получилось диапазон имеет значения:%n from = %f%n to = %f%n", range.getFrom(), range.getTo());
+        System.out.printf("Длина диапазона = %f%n", range.getLength());
+
+        System.out.println("И теперь проверим принадлежность числа к диапазону.");
+        System.out.print("Введите число для проверки = ");
+        double number = scanner.nextDouble();
+
+        if (range.isInside(number)) {
+            System.out.printf("Число %f принадлежит диапазону (%f;%f)", number, range.getFrom(), range.getTo());
+        } else {
+            System.out.printf("Число %f не принадлежит диапазону (%f;%f)", number, range.getFrom(), range.getTo());
+        }
     }
 }
